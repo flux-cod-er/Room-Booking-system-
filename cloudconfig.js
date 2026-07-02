f7ef3188e24d45c716b668734c88428217f96be4
flux <ssumit0420@gmail.com>
2026-07-02 14:35:49 +0530

    Fixed project compatibility issues

--- cloudconfig.js
+++ cloudconfig.js
@@ -1,6 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
+
+
+
 cloudinary.config({       //to add backend to cloudinary we need to configure it
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
@@ -11,7 +14,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'wanderlust_DEV', // The folder in your Cloudinary account where the images will be stored
-    allowerdFormats: ['jpg', 'png', 'jpeg'], // Allowed file formats
+    allowedFormats: ['jpg', 'png', 'jpeg'], // Allowed file formats
   },
 });
 
